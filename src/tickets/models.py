e44c2174fe333a5d83992a16233898d3a81a3f1f
vityoknumber1 <viktorkysil1997@gmail.com>
2023-06-28 22:46:52 +0300

    Reassign and take PUT methods added

--- src/tickets/models.py
+++ src/tickets/models.py
@@ -31,9 +31,10 @@ class Message(models.Model):
         on_delete=models.RESTRICT,
         related_name="messages",  # noqa
     )
-    request = models.ForeignKey(
+    ticket = models.ForeignKey(
         "tickets.Ticket", on_delete=models.RESTRICT, related_name="messages"
     )
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "messages"
