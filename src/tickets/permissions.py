27f8e7cecf09cecccc5c3770fe48d30bbcb0d643
vityoknumber1 <viktorkysil1997@gmail.com>
2023-07-08 09:41:30 +0300

    Added permissions and code for ticket reassign

--- src/tickets/permissions.py
+++ src/tickets/permissions.py
@@ -1,7 +1,9 @@
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import BasePermission
 
 from tickets.models import Ticket
 from users.constants import Role
+from users.models import User
 
 
 class RoleIsAdmin(BasePermission):
@@ -25,3 +27,37 @@ class IsOwner(BasePermission):
 
     def has_object_permission(self, request, view, obj: Ticket):
         return obj.user == request.user
+
+
+class UserIsNewManager(BasePermission):
+    def has_permission(self, request, view):
+        new_manager_id = request.data["new_manager"]
+        ticket_id = request.parser_context["kwargs"]["pk"]
+
+        try:
+            ticket = Ticket.objects.get(id=ticket_id)
+            user = User.objects.get(id=new_manager_id)
+        except Ticket.DoesNotExist:
+            raise PermissionDenied(
+                f"Ticket with ID {ticket_id} \
+                                   does not exist."
+            )
+        except User.DoesNotExist:
+            raise PermissionDenied(
+                f"User with ID {new_manager_id} \
+                                   does not exist."
+            )
+
+        if user.role != Role.MANAGER:
+            raise PermissionDenied(
+                f"User with ID {new_manager_id} \
+                                   is not a manager."
+            )
+
+        if ticket.manager_id == new_manager_id:
+            raise PermissionDenied(
+                "Cannot assign current manager \
+                                   as the new manager."
+            )
+
+        return True
